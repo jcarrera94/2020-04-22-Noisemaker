70f22c90274de94f3e8924ea9b4b74668c3f7c7f
Juan Carrera <juankarrera@gmail.com>
2020-04-22 15:18:00 -0400

    Updates

---
+++ precache-manifest.baf306087c25946149f7b1261a8309bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "473410cc9e94b802eb76d59b0747e650",
+    "url": "/2020-04-22-Noisemaker/index.html"
+  },
+  {
+    "revision": "085b924135d64504e028",
+    "url": "/2020-04-22-Noisemaker/static/css/main.ca0cab7e.chunk.css"
+  },
+  {
+    "revision": "20861c451ac7a10be40b",
+    "url": "/2020-04-22-Noisemaker/static/js/2.10144755.chunk.js"
+  },
+  {
+    "revision": "b28590a4d5a036be9947256ff9f1f35b",
+    "url": "/2020-04-22-Noisemaker/static/js/2.10144755.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "085b924135d64504e028",
+    "url": "/2020-04-22-Noisemaker/static/js/main.34bd4e49.chunk.js"
+  },
+  {
+    "revision": "6a8f76f939c37f613ecf",
+    "url": "/2020-04-22-Noisemaker/static/js/runtime-main.a6604fd4.js"
+  }
+]);
